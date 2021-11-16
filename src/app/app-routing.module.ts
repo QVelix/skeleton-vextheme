5317ccb6ea5976ec930af16d94c75aae5d5e175a
QVelix <39808785+QVelix@users.noreply.github.com>
2021-11-16 15:31:14 +0400

    Создание Dashboard компонента и подключение его
    Здесь будут отображаться категории товаров

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuicklinkStrategy } from 'ngx-quicklink';
 import { CustomLayoutComponent } from './custom-layout/custom-layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CustomLayoutComponent,
-    children: []
+    children: [
+      {
+        path: 'dashboard',
+        redirectTo: '/',
+      },
+      {
+        path: '',
+        loadChildren: ()=>import('./dashboard/dashboard.module').then(m=>m.DashboardModule)
+      }
+    ]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    // preloadingStrategy: PreloadAllModules,
     scrollPositionRestoration: 'enabled',
     relativeLinkResolution: 'corrected',
     anchorScrolling: 'enabled'

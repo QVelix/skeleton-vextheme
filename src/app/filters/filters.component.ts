55691644186c7db50ab2a498cd8c3dc97c54297c
QVelix <39808785+QVelix@users.noreply.github.com>
2022-01-11 12:20:36 +0400

    Небольшая логика для отображения текста
    Текст не отображается

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -10,6 +10,9 @@ import { FiltersGetterService } from '../Services/filters-getter.service';
 export class FiltersComponent implements OnInit {
   serviceType:string;
   typeElSignatures=["ЭЛЕКТРОННЫЕ ТОРГИ", "ГОСУСЛУГИ И ОТЧЁТНОСТЬ", "СИСТЕМЫ ЭДО", "ФИЗИЧЕСКИМ ЛИЦАМ", "ПОПУЛЯРНОЕ"];
+  nameAcBtns=[L1_B1=>'ИП', L1_B2=>'ЮЛ',L1_B3=>'Группа компаний', L1_B4=>'Бюджетное учреждение',L1_B5=>'Налоговый представитель',
+    L2_B1=>'Онлайн', L2_B2=>'Локальный комплекс',L2_B3=>'Решение, встроенное в 1С',
+    L3_B1=> 'УСН, ПСН, ЕСН', L3_B2=> 'ОСНО',L3_B3=> 'Нулевая отчётность'];
   selectedType;
   typeAccounting;
 
@@ -18,7 +21,17 @@ export class FiltersComponent implements OnInit {
     console.log(this.selectedType);
   }
   log(info){
-    console.log(info);
+    //console.log(info);
+  }
+  filterLoadEv(keyL2,keyL1){
+    //console.log(keyL2, keyL1);
+    let text:any;
+    Object.keys(this.nameAcBtns).forEach((key)=>{
+        if(keyL2==key){text = this.nameAcBtns[key];
+      }
+    });
+    console.log(text);
+    document.querySelector('.'+keyL1+' > div > .'+keyL2).textContent = text;
   }
 
   constructor(private route: ActivatedRoute, private filterGetter: FiltersGetterService) {
